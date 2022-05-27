af3cbce9a4a3bf6eddf43bfd8eb57536e79cc24b
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-05-27 14:15:28 +0900

    google-cloud-bigqueryを用いてpoetry run使用時のGithub Actionsのsecretをテスト

--- poetry_env_test.py
+++ poetry_env_test.py
@@ -1,5 +1,17 @@
+import base64
 import os
 
-print(os.environ.get('test'))
-print(os.environ.get('POETRY_test'))
-print(os.environ.get('POETRY_TEST'))
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+cred = eval(base64.b64decode(os.enviropn.get('GCP_SERVICE_KEY')))
+
+credentials = service_account.Credentials.from_service_account_info(
+    cred,
+    scopes = ['https://www.googleapis.com/auth/bigquery']
+)
+
+client = bigquery.Client(
+    project='voltaic-country-281210',
+    credentials=credentials
+)
